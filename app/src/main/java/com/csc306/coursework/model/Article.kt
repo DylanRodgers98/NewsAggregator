c88a2d14afabe24bfafb573e906fbc540276f89d
Dylan Rodgers <dylanirodgers@aol.com>
2020-11-15 18:02:27 +0000

    Move title analysis to model class

--- app/src/main/java/com/csc306/coursework/model/Article.kt
+++ app/src/main/java/com/csc306/coursework/model/Article.kt
@@ -1,6 +1,10 @@
 package com.csc306.coursework.model
 
+import com.google.cloud.language.v1beta2.Document
+import com.google.cloud.language.v1beta2.Entity
+import com.google.cloud.language.v1beta2.LanguageServiceClient
 import java.time.LocalDateTime
+import java.util.*
 
 class Article(
     val source: String,
@@ -10,5 +14,23 @@ class Article(
     val description: String,
     val articleURL: String
 ) {
-    var titleKeywords: Map<String, Float>? = null
+    private var titleKeywords: Map<String, Float>? = null
+
+    fun getTitleKeywords(): Map<String, Float> {
+        if (titleKeywords == null) {
+            analyseArticleTitle()
+        }
+        return titleKeywords!!
+    }
+
+    private fun analyseArticleTitle() {
+        LanguageServiceClient.create().use { client ->
+            val document: Document = Document.newBuilder()
+                .setContent(title.toLowerCase(Locale.getDefault()))
+                .setType(Document.Type.PLAIN_TEXT)
+                .build()
+            val entities: List<Entity> = client.analyzeEntities(document).entitiesList
+            titleKeywords = entities.associateBy({ it.name }, { it.salience })
+        }
+    }
 }
